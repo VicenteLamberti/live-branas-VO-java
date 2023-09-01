abe614760315eba2ba87e88609ded0189985a170
Vicente Leonardo De Souza Lamberti <vicente.lamberti@hotmail.com>
2023-09-01 12:04:35 -0300

    Iniciando teste parking service

--- src/test/java/br/com/vicente/ParkingServiceTest.java
+++ src/test/java/br/com/vicente/ParkingServiceTest.java
@@ -1,5 +1,6 @@
 package br.com.vicente;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,6 +8,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class ParkingServiceTest {
 
     @Test
-    void dev
+    void deveEntrarESairDoEstacionamentoCalculandoOValorDaTarifa(){
+        var parkingService = new ParkingService();
+        var plate = "AAA999";
+        var expectedTicketPrice = 20;
+        parkingService.checkin(plate);
+        var ticket = parkingService.checkout(plate);
+
+        Assertions.assertEquals(expectedTicketPrice, ticket.price());
+    }
 
 }
