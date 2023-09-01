636eadb4cfc81a8b84421e316028870f49033dd9
Vicente Leonardo De Souza Lamberti <vicente.lamberti@hotmail.com>
2023-09-01 12:01:31 -0300

    criacao projeto

---
+++ src/main/java/br/com/vicente/ParkingService.java
@@ -0,0 +1,6 @@
+package br.com.vicente;
+
+public class ParkingService {
+
+
+}
---
+++ src/test/java/br/com/vicente/ParkingServiceTest.java
@@ -0,0 +1,12 @@
+package br.com.vicente;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParkingServiceTest {
+
+    @Test
+    void dev
+
+}
